2df7fd8de334d6ea79fc099f983c0c88e9ccdb2a
wilinz <weiizan71@gmail.com>
2023-01-31 08:57:23 +0800

    update login ui

--- lib/ui/page/login/login.dart
+++ lib/ui/page/login/login.dart
@@ -68,7 +68,9 @@ class _LoginPageState extends State<_LoginPage> {
                             hintText: "您的用户名",
                             prefixIcon: Icon(Icons.person),
                             // helperText: '用户名',
-                            border: const OutlineInputBorder(),
+                            border: const OutlineInputBorder(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(16))),
                           ),
                           validator: (v) {
                             return v!.trim().length > 0 ? null : "账号不能为空";
@@ -90,7 +92,7 @@ class _LoginPageState extends State<_LoginPage> {
                                 _passwordVisible
                                     ? Icons.visibility
                                     : Icons.visibility_off,
-                                color: Theme.of(context).primaryColorDark,
+                                color: Theme.of(context).primaryColor,
                               ),
                               onPressed: () {
                                 //更新状态控制密码显示或隐藏
@@ -100,7 +102,9 @@ class _LoginPageState extends State<_LoginPage> {
                               },
                             ),
                             // helperText: '密码',
-                            border: const OutlineInputBorder(),
+                            border: const OutlineInputBorder(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(16))),
                           ),
                           validator: (v) {
                             return v!.trim().length > 0 ? null : "密码不能为空";
@@ -130,7 +134,7 @@ class _LoginPageState extends State<_LoginPage> {
                         //   },
                         // ),
                         Container(
-                          height: 16,
+                          height: 32,
                         ),
                         Consumer<LoginViewModel>(
                           builder: (context, loginViewModel, child) {
